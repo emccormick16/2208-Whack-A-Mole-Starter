4e4aa36f5e6974490057270a9a980435b2f0ca8e
Ed McCormick <edward.mccormick3@gmail.com>
2022-09-05 17:32:15 -0500

    added difficulties

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ function showMole() {
   holes[randomNum].classList.toggle("mole");
 }
 
-setInterval(showMole, 1000);
+let difficulty = document.getElementsByClassName("difficulty")[0];
 
-let whackamole = document.getElementById("whack-a-mole");
+difficulty.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.matches("#easy")) {
+    setInterval(showMole, 2000);
+  }
+  if (clickEvent.target.matches("#medium")) {
+    setInterval(showMole, 1000);
+  }
+  if (clickEvent.target.matches("#hard")) {
+    setInterval(showMole, 300);
+  }
+});
 
 function updateScore() {
   let scoreid = document.getElementById("score");
   scoreid.innerHTML = score;
 }
+let whackamole = document.getElementById("whack-a-mole");
 
 whackamole.addEventListener("click", (clickEvent) => {
   if (clickEvent.target.matches(".mole")) {
