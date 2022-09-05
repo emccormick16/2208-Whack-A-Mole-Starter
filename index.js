4e561b8170df605ca94cd3c7cc35e4102055c134
Ed McCormick <edward.mccormick3@gmail.com>
2022-09-05 16:33:28 -0500

    added javascript

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 let score = 0;
+
+function showMole() {
+  let holes = document.getElementsByClassName("hole");
+  let randomNum = Math.floor(Math.random() * 9);
+  holes[randomNum].classList.toggle("mole");
+}
+
+setInterval(showMole, 100);
+
+let whackamole = document.getElementById("whack-a-mole");
+
+function updateScore() {
+  let scoreid = document.getElementById("score");
+  scoreid.innerHTML = score;
+}
+
+whackamole.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.matches(".mole")) {
+    score++;
+    updateScore();
+  }
+});
